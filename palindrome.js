f1e33e9f551b4886804bd9918e3af1001f534959
Moumita <moupriya2609@gmail.com>
2023-01-03 20:15:11 +0530

    palindrome problem

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+
+let str="madam";
+let rev="";
+  for (let i=str.length-1;i>=0;i++){
+      rev=rev+str[i];
+  }
+ if(rev==str){
+     console.log("palindrome");
+ }
+ else{
+     console.log("Not palindrome");
+ }
